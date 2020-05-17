7709598bd7f1759abc874f8deb4f228efe991d8a
Mathias Karlsson <Mathias.Karlsson@xlent.se>
2020-05-17 22:51:12 +0200

    Add ServiceCollectionExtensions (AddServicesByAttribute)

---
+++ src/server/TicTacFoo.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using TicTacFoo.Logic.Attributes;
+
+namespace TicTacFoo.Api.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddServicesByAttribute(this IServiceCollection services)
+        {
+            RegisterWithAttribute(ref services, typeof(SingletonAttribute));
+            RegisterWithAttribute(ref services, typeof(TransientAttribute));
+            RegisterWithAttribute(ref services, typeof(ScopedAttribute));
+        }
+
+        private static void RegisterWithAttribute(ref IServiceCollection services, Type injectableAttribute)
+        {
+            var loadedAssemblies = AppDomain.CurrentDomain
+                .GetAssemblies()
+                .ToList();
+            var loadedPaths = loadedAssemblies
+                .Select(a => a.Location)
+                .ToArray();
+
+            var referencedPaths = Directory
+                .GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll");
+            var toLoad = referencedPaths
+                .Where(r => !loadedPaths
+                    .Contains(r, StringComparer.InvariantCultureIgnoreCase))
+                .ToList();
+            toLoad
+                .ForEach(path => loadedAssemblies
+                    .Add(AppDomain.CurrentDomain
+                        .Load(AssemblyName.GetAssemblyName(path))));
+
+            foreach (var assembly in loadedAssemblies)
+            {
+                var allTypes = assembly
+                    .GetTypes();
+
+                var classesWithAttribute = allTypes
+                    .Where(t => t.IsClass && t.CustomAttributes
+                        .Any(a => a.AttributeType == injectableAttribute));
+
+                foreach (var implementationType in classesWithAttribute)
+                {
+                    var interfaceType = allTypes
+                        .FirstOrDefault(t => t.IsInterface && t.Name.Substring(1) == implementationType.Name);
+
+                    if (interfaceType == null)
+                        throw new Exception(
+                            $"Failed to resolve interface for class {implementationType.Name}, unable to inject dependency!");
+
+                    if (injectableAttribute == typeof(TransientAttribute))
+                        services
+                            .AddTransient(interfaceType, implementationType);
+                    else if (injectableAttribute == typeof(ScopedAttribute))
+                        services
+                            .AddScoped(interfaceType, implementationType);
+                    else if (injectableAttribute == typeof(SingletonAttribute))
+                        services
+                            .AddSingleton(interfaceType, implementationType);
+                }
+            }
+        }
+    }
+}
