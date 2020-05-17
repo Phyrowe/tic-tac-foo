5f635db4579e18e90f66b26bb8c8d7dab80326e7
Mathias Karlsson <Mathias.Karlsson@xlent.se>
2020-05-17 22:52:16 +0200

    TicTacFoo.Api: Add attribute dependency to startup (AddServicesByAttribute)

--- src/server/TicTacFoo.Api/Startup.cs
+++ src/server/TicTacFoo.Api/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TicTacFoo.Api.Extensions;
 
 namespace TicTacFoo.Api
 {
@@ -36,6 +37,7 @@ namespace TicTacFoo.Api
             });
             services.AddCors();
             services.AddHttpContextAccessor();
+            services.AddServicesByAttribute();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
