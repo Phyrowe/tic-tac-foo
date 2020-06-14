dfc82bf202a9b26be2815bdebfac732b0d800ac4
Mathias Karlsson <Mathias.Karlsson@xlent.se>
2020-06-14 23:30:21 +0200

    Start implement mithril client.

--- src/client/src/components/App.jsx
+++ src/client/src/components/App.jsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import m from 'mithril';
+import {appRoutes} from '../appRoutes';
+import {connectSignalRHub} from '../application/hub/signalr';
+import {compose} from 'lodash/fp';
 
-export const App = () => {
-    return(<div>Tic tac foo :-)</div>)
-};
+export const App = () => ({
+    oncreate: ({attrs: {settings, state, actions}, dom}) => {
+        compose(actions.setHub, connectSignalRHub)(settings.hub.url);
+        m.route.prefix = settings.routing.prefix;
+        m.route(dom, settings.routing.rootPath, appRoutes(state, actions));
+    },
+    view: ({ children }) => (
+        <div class="App">
+            {children}
+        </div>
+    )
+});
